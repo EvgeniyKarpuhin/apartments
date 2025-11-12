52f46c9506e97f13d20c28e4070586fffcca78a4
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-11-12 19:20:15 +0200

    Тест для ios

--- src/main.js
+++ src/main.js
@@ -8,12 +8,14 @@ import router from './router'
 import lazyLoad from './directives/lazyLoad'
 
 function setRealHeight() {
-  const vh = window.innerHeight * 0.01;
+  const vh = (document.documentElement.clientHeight || window.innerHeight) * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
 setRealHeight();
 window.addEventListener('resize', setRealHeight);
+window.addEventListener('scroll', setRealHeight); //Добавил
+window.addEventListener('orientationchange', setRealHeight); //Добавил
 
 const app = createApp(App)
 
