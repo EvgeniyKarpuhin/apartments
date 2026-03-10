c99a8430e22e436efccf505cc213d351e787cb9c
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2026-03-10 12:15:46 +0200

    add fix

--- src/main.js
+++ src/main.js
@@ -42,6 +42,13 @@ window.addEventListener('resize', setRealHeight);
 //   window.visualViewport.addEventListener('scroll', setRealHeight);
 // }
 
+const urlParams = new URLSearchParams(window.location.search);
+const redirect = urlParams.get("redirect");
+
+if(redirect) {
+  window.history.replaceState(null, null, redirect);
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
